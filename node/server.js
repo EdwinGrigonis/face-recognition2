f0f4042460dcabfffbd42ca813db8f90be37429e
GenoSamuel <truegenosamuel@gmail.com>
2022-03-02 11:38:47 +0900

    updated server code

--- node/server.js
+++ node/server.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.get('/', (req, res) => {
+    res.send('this is working');
+})
+
+app.listen(3000, () => {
+    console.log("app is running");
+});
