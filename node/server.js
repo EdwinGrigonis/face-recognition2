062be0597b96a909d2631be44eaf97a264bcb712
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-03-07 12:49:17 +0900

    updated registration

--- node/server.js
+++ node/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 const cors = require('cors');
 const knex = require('knex')({
@@ -52,15 +53,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password} = req.body;
-    knex('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
-        }).then(user => {
-            res.json(user[0]);
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    knex.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email            
         })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: email,
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })  
     .catch(err => res.status(400).json('Unable to register'));
 })
 
