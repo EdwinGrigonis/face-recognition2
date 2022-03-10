b6c06c1ee6b5919b10a952e5599004c54fc93421
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-03-10 10:05:10 +0900

    changed server directories

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
         this.state.input)
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://blooming-shore-81254.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
